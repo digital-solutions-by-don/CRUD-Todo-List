21fa75adf6b7deefefd75bddaca8f0ed7ff01cfc
Donald Whitely <dswhitely1@gmail.com>
2019-09-12 08:27:01 -0400

    App Rendering

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
